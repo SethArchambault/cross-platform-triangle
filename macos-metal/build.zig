8c43913c5dc2c736d3bc73e560403cf5caf7124a
SethArchambault <seth@doercreator.com>
2024-01-07 19:18:45 -0500

    zig build

---
+++ macos-metal/build.zig
@@ -0,0 +1,44 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const exe = b.addExecutable(.{
+        .name = "main",
+    });
+    //exe.addIncludePath("includes");
+    exe.addCSourceFiles(&.{
+        "main.cpp",
+    }, &.{
+        "-std=c++20",
+        "-Werror",
+        "-Wall",
+        "-Wextra",
+        "-Wshadow",
+        "-Wconversion",
+        "-Wno-unused-variable",
+        "-Wno-unused-parameter",
+        "-Wno-deprecated-declarations",
+        "-Wno-unused-value",
+        "-ferror-limit=4",
+        "-O1",
+        "-o temp/main",
+    });
+    exe.linkLibC();
+    exe.linkLibCpp();
+    exe.linkSystemLibrary("c++");
+    exe.linkFramework("Metal");
+    exe.linkFramework("Foundation");
+    exe.linkFramework("Cocoa");
+    exe.linkFramework("CoreGraphics");
+    exe.linkFramework("MetalKit");
+    b.installArtifact(exe);
+
+    const run_cmd = b.addRunArtifact(exe);
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    // `zig build run -- arg1 arg2 etc`
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+}

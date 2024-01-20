14b3c5715a38f7999c4b371753bb50368c9a6eb0
SethArchambault <seth@doercreator.com>
2024-01-20 17:37:47 -0500

    rotation for constantanimation

--- main.cpp
+++ main.cpp
@@ -50,29 +50,34 @@ void draw_arrow(String * id_str, V2F32 pos, F32 width, F32 length, F32 turns, V3
 }
 
 
-F32 rotation = 0.0;
-
-void game_mouse_move(S32 x, S32 y) {
-    //rotation = (F32) x / 300.0f;
-}
-
 struct State {
     B32 initialized;
     Arena * arena;
+    F32 rotation;
 };
 State __s;
 
+
+
+void game_mouse_move(S32 x, S32 y) {
+    //__s.rotation = (F32) x / 300.0f;
+}
 S32 game_loop() {
     if (!__s.initialized) {
         __s.arena = arena_init();
         __s.initialized = 1;
+        __s.rotation = 0.0f;
     }
     V3F32 color = {{200.0}, {0.0}, {150.0}};
     for (S32 idx = 0; idx < 50; idx++) {
         String *id_str = string_make(__s.arena, "basic_triangle", idx);
-        draw_arrow(id_str, {idx*50.0f, idx*25.0f}, 50.0f, 70.0f, rotation+(F32)idx*0.1f, color);
+        draw_arrow(id_str, {idx*50.0f, idx*25.0f}, 50.0f, 70.0f, __s.rotation+(F32)idx*0.1f, color);
     }
-    rotation += 1.0f;
+    __s.rotation += 0.01f ;
+    if (__s.rotation > 2.0f) {
+        __s.rotation = 0.0f;
+    }
+    printf("rotation %f\n", __s.rotation);
     return 0;
 }
 
